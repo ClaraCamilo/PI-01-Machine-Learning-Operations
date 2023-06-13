ca80213fcfe3100c6e63c2a61727604de3f39a6c
ClaraCamilo <123392474+ClaraCamilo@users.noreply.github.com>
2023-06-13 14:11:53 -0300

    ruta modelo arreglada

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ df = pd.read_csv('database/base_datos.csv')
 df_modelo = pd.read_csv("database/modelo.csv")
 df_modelo1 = df_modelo.head(2000).drop(["title"], axis=1)
 
-with open("database\cosine.pkl", "rb") as archivo:
+with open("database/cosine.pkl", "rb") as archivo:
     cosine_sim = pickle.load(archivo)
     
 #instancia de fastApi
